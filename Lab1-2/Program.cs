7b6b293b53b1ff5b64c8013fc2921cd4426f2f16
Anton Bessarab <antonbessarab55@gmail.com>
2023-09-11 19:06:23 +0300

    Добавьте файлы проекта.

---
+++ Lab1-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Lab1_2
+{
+    public class RandomNumber
+    {
+        public static byte[] GenerateRandomNumber(int length)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                string randomNumber = Convert.ToBase64String(RandomNumber.GenerateRandomNumber(32));
+                Console.WriteLine(randomNumber);
+            }
+            Console.ReadLine();
+        }
+    }
+}
