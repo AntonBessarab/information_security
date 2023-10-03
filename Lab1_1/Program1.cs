2e09aa4232de6a356fe1b3dd6816e0580a3a2678
Anton Bessarab <antonbessarab55@gmail.com>
2023-10-03 09:08:56 +0300

    ...

--- Lab1_1/Program1.cs
+++ Lab1_1/Program1.cs
@@ -10,11 +10,22 @@ namespace Lab1_1
 
             Random random1 = new Random(1234);
 
+            Random random2 = new Random(1234);
+
+            Console.WriteLine("Послідовність 1");
             for (int i = 0; i < 10; i++)
             {
                 int rnd1 = random1.Next(0, 10);
 
                 Console.WriteLine(rnd1);
+
+            }
+            Console.WriteLine("Послідовність 2");
+            for (int b = 0; b < 10; b++)
+            {
+                int rnd2 = random2.Next(0, 10);
+
+                Console.WriteLine(rnd2);
 
             }
         }
--- Lab2_1/Program.cs
+++ Lab2_1/Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab2_1
+class VernamCipher
 {
-    internal class Program
+    // Метод для шифрування тексту за допомогою шифру Вернама
+    public static byte[] Encrypt(byte[] plaintextBytes, byte[] keyBytes)
     {
-        static void Main(string[] args)
+        if (plaintextBytes.Length != keyBytes.Length)
         {
+            throw new ArgumentException("Довжина ключа повинна бути рівною довжині тексту");
+        }
+
+        byte[] ciphertextBytes = new byte[plaintextBytes.Length];
+
+        for (int i = 0; i < plaintextBytes.Length; i++)
+        {
+            ciphertextBytes[i] = (byte)(plaintextBytes[i] ^ keyBytes[i]);
+        }
+
+        return ciphertextBytes;
+    }
+
+    static void Main()
+    {
+        string inputFilePath = "C:\\Users\\anton\\source\\inf.bez\\Lab1_1\\Lab2_1\\Text.txt";
+
+        byte[] plaintextBytes = File.ReadAllBytes(inputFilePath);
+
+        string password = "oyuantdi"; 
+
+        // Кодуємо пароль в байтовий масив
+        byte[] keyBytes = Encoding.UTF8.GetBytes(password);
+
+        try
+        {
+            // Шифруємо текст
+            byte[] encryptedBytes = Encrypt(plaintextBytes, keyBytes);
+
+            // Змінюємо розширення файлу на .dat
+            string outputFilePath = Path.ChangeExtension(inputFilePath, ".dat");
+
+            // Записуємо зашифровані байти у новий файл
+            File.WriteAllBytes(outputFilePath, encryptedBytes);
+
+            Console.WriteLine("Текст був успішно зашифрований і збережений у файлі " + outputFilePath);
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine("Помилка: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Під час виконання сталася помилка: " + ex.Message);
         }
     }
 }
